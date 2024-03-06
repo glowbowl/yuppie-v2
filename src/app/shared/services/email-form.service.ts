cefd258cc2eb4d994187a9439b8bb9cbec7a7253
Vlad Lyashko <vlad.lyashko@kindgeek.com>
2024-03-06 02:13:20 +0200

    mail sending update dynamic

--- src/app/shared/services/email-form.service.ts
+++ src/app/shared/services/email-form.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -12,12 +12,17 @@ export class EmailFormService {
   constructor(private http: HttpClient) {}
 
   public sendEmail(input: any) {
-    // const headers = new HttpHeaders({
-    //   'Content-Type': 'application/json',
-    //   'Accept': 'application/json'
-    // });
-    // const options = { headers: headers };
-    // return this.http.post(this.url, input, options);
-    return this.http.post('../assets/php/email.php', input);
+    const headers = new HttpHeaders().set(
+      'Content-Type',
+      'application/x-www-form-urlencoded; charset=UTF-8'
+    );
+    const options = { headers: headers };
+    const body = new HttpParams()
+      .set('fullName', input.fullName)
+      .set('phone', input.phone)
+      .set('company', input.company)
+      .set('mail', input.mail);
+    return this.http.post('../assets/php/email.php', body, options);
+    // return this.http.post('http://127.0.0.1:8000/email.php', body, options);
   }
 }
